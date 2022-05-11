1b5d50c68d95418f32a272fda9320b814ce28027
AndrejApa <81564427+AndrejApa@users.noreply.github.com>
2022-05-11 17:13:41 +0300

    "taks"

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,9 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
-	return
+	var x float32 = 0
+	for _, num := range input {
+		x += num
+	}
+	return x / float32(len(input))
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,19 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	var i int = 0
+	key := make([]int, len(input))
+	for k := range input {
+		key[i] = k
+		i++
+	}
+	sort.Ints(key)
+	for _, b := range key {
+		result = append(result, input[b])
+
+	}
+	return result
 }
